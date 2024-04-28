f206b4fe70061dba55d07c48a8a9d99014bedd47
juwel-hub <jr3764920@gmail.com>
2024-04-28 21:08:37 +0600

    data created on data base

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const travelCollection = client.db("travelDB").collection("travel");
+
+    app.get("/travels", async (req, res) => {
+      console.log(object);
+    });
+    app.post("/travels", async (req, res) => {
+      const newTravel = req.body;
+      console.log(newTravel);
+      const result = await travelCollection.insertOne(newTravel);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -31,7 +42,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
